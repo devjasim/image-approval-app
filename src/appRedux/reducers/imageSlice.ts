9028577bcf083cfd45d14b53da6a83c1af65f60a
devjasim <jasim.eng360@gmail.com>
2022-02-21 20:34:10 +0600

    refector iamge slice reducer

--- src/appRedux/reducers/imageSlice.ts
+++ src/appRedux/reducers/imageSlice.ts
@@ -4,15 +4,24 @@ import {
   REJECTE_IMAGE,
 } from "../actions/actionTypes";
 
+// Approved List and Rejected List state
 const initialState = {
   approvedList: [],
   rejectedList: [],
 };
 
+// Get Image Random image state
 const getState = {
   thumbnail: undefined,
 };
 
+/**
+ * @name imageSLiceReducer
+ * @description Image slice reducer set approvedImagelist either rejectImagelist depends one which action calls
+ * @param state
+ * @param action
+ * @returns When Action Type APPROVED return return approved image list with new item
+ */
 export const imageSLiceReducer = (state = initialState, action: any) => {
   switch (action.type) {
     case APPROVED_IMAGE:
@@ -30,6 +39,13 @@ export const imageSLiceReducer = (state = initialState, action: any) => {
   }
 };
 
+/**
+ * @name getImageReducer
+ * @description get image reducer call for load random image and set thumbnail iamge to the state
+ * @param state
+ * @param action
+ * @returns return thumbnail with new playload data
+ */
 export const getImageReducer = (state = getState, action: any) => {
   switch (action.type) {
     case GET_IMAGE:
