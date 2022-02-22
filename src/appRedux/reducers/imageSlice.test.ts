71429bbe3a55533f28ae98293e591cc90854455c
devjasim <jasim.eng360@gmail.com>
2022-02-22 18:26:14 +0600

    Update all test case for imageSlice reducer

--- src/appRedux/reducers/imageSlice.test.ts
+++ src/appRedux/reducers/imageSlice.test.ts
@@ -1,3 +1,4 @@
+import { APPROVED_IMAGE, REJECTE_IMAGE } from "./../actions/actionTypes";
 import { imageSLiceReducer } from "./imageSlice";
 
 describe("Image Approved Reducer", () => {
@@ -6,8 +7,34 @@ describe("Image Approved Reducer", () => {
       approvedList: [],
       rejectedList: [],
     };
-    const action = { type: "" };
-    const result = imageSLiceReducer(initialState, action);
+    const action = { type: APPROVED_IMAGE };
+    const result = imageSLiceReducer(initialState, action.type);
     expect(result).toEqual({ approvedList: [], rejectedList: [] });
+  });
+
+  it("Should return receiving payload for approved state", () => {
+    const initialState = {
+      approvedList: [],
+      rejectedList: [],
+    };
+    const post = { id: "Id", url: "url" };
+    const result = imageSLiceReducer(initialState, {
+      type: APPROVED_IMAGE,
+      payload: post,
+    });
+    expect(result).toEqual({ approvedList: [post], rejectedList: [] });
+  });
+
+  it("Should return receiving payload for rejected state", () => {
+    const initialState = {
+      approvedList: [],
+      rejectedList: [],
+    };
+    const post = { id: "Id", url: "url" };
+    const result = imageSLiceReducer(initialState, {
+      type: REJECTE_IMAGE,
+      payload: post,
+    });
+    expect(result).toEqual({ approvedList: [], rejectedList: [post] });
   });
 });
